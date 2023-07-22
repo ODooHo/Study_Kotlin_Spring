042952d2ab8a404dc47d717b0a9df77c2af2884a
engh0205@naver.com <engh0205@naver.com>
2023-07-22 23:55:11 +0900

    Spring

---
+++ MVC_2/validation/src/main/java/hello/itemservice/web/validation/form/ItemUpdateForm.java
@@ -0,0 +1,25 @@
+package hello.itemservice.web.validation.form;
+
+import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Data
+public class ItemUpdateForm {
+
+    @NotNull
+    private Long id;
+
+    @NotBlank
+    private String itemName;
+
+    @NotNull
+    @Range(min = 1000, max = 1000000)
+    private Integer price;
+
+
+    //수정에선 수량 변경 제한 x
+    private Integer quantity;
+}
