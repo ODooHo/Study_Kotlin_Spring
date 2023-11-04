18db56f0ff63a237a5a5247cf95ae1a2de78ad6d
engh0205@naver.com <engh0205@naver.com>
2023-11-04 17:04:17 +0900

    Kotlin

---
+++ sampleKotlin/src/main/kotlin/studio/aroundhub/kotlin/variable/VariableSample.kt
@@ -0,0 +1,19 @@
+package studio.aroundhub.kotlin.variable
+
+class VariableSample {
+
+    companion object{
+        @JvmStatic
+        fun main(args: Array<String>){
+            val name : String = "ODooHo"
+            val org = "Around Hub Studio"
+            var age = 12
+
+            age = 15
+
+            println("my name is $name")
+            println("my org is $org")
+            println("my age is $age")
+        }
+    }
+}
