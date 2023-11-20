d9245c8f9cd410a04f66e3bbd97a1ff6c0814b64
engh0205@naver.com <engh0205@naver.com>
2023-11-20 11:39:35 +0900

    QClass 생성

---
+++ board_dsl/src/main/generated/com/dooho/board/entity/QUserEntity.java
@@ -0,0 +1,47 @@
+package com.dooho.board.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QUserEntity is a Querydsl query type for UserEntity
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QUserEntity extends EntityPathBase<UserEntity> {
+
+    private static final long serialVersionUID = 516309569L;
+
+    public static final QUserEntity userEntity = new QUserEntity("userEntity");
+
+    public final StringPath userAddress = createString("userAddress");
+
+    public final StringPath userEmail = createString("userEmail");
+
+    public final StringPath userNickname = createString("userNickname");
+
+    public final StringPath userPassword = createString("userPassword");
+
+    public final StringPath userPhoneNumber = createString("userPhoneNumber");
+
+    public final StringPath userProfile = createString("userProfile");
+
+    public QUserEntity(String variable) {
+        super(UserEntity.class, forVariable(variable));
+    }
+
+    public QUserEntity(Path<? extends UserEntity> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QUserEntity(PathMetadata metadata) {
+        super(UserEntity.class, metadata);
+    }
+
+}
+
